1feefc9620ca2d27e81443e22e7e6217c6b3e937
keisuke watanabe <kskwtnb93@gmail.com>
2023-10-09 16:13:59 +0900

    ⚙️ 未使用の import 文を削除／import 文の並び替え

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,31 @@ module.exports = {
     "@typescript-eslint/no-unsafe-member-access": "error",
     "@typescript-eslint/no-unsafe-return": "error",
     "unused-imports/no-unused-imports-ts": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "{react,react-dom/**,react-router-dom}",
+            group: "builtin",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
   },
 };
