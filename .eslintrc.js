73c573bfecf30697b042634c4a019e99b1a49053
keisuke watanabe <kskwtnb93@gmail.com>
2023-10-09 16:12:24 +0900

    ⚙️ eslint 設定

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: ["plugin:@typescript-eslint/recommended", "next/core-web-vitals"],
+  plugins: ["unused-imports"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unsafe-call": "error",
+    "@typescript-eslint/no-unsafe-member-access": "error",
+    "@typescript-eslint/no-unsafe-return": "error",
+    "unused-imports/no-unused-imports-ts": "warn",
+  },
+};
---
+++ src/components/sample.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+type SampleProps = {
+  name: string;
+};
+export const Sample: FC = ({ user }) => {
+  return <div>{user.name}</div>;
+};
