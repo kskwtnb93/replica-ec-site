27a4738318d042e380c95295bc85819fc03c79bc
keisuke watanabe <kskwtnb93@gmail.com>
2023-10-09 23:56:14 +0900

    🗑️ 不要ファイル削除

--- src/components/sample.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-import styles from '@/components/Sample.module.scss'
-
-const Sample: FC = () => {
-  return <div className={styles.sample}>Sample</div>
-}
-
-export default Sample
