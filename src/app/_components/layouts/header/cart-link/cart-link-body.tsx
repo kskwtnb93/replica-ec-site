21d7411a28805c91269d8105e74ade0517f9d727
keisuke watanabe <kskwtnb93@gmail.com>
2023-12-23 21:26:07 +0900

    🐞 ヘッダー：カート個数が0の時の表示を修正（close #15）

--- src/app/_components/layouts/header/cart-link/cart-link-body.tsx
+++ src/app/_components/layouts/header/cart-link/cart-link-body.tsx
@@ -18,36 +18,38 @@ export default function CartLinkBody() {
         position: relative;
       `}
     >
-      <span
-        className={css`
-          position: absolute;
-          top: calc(-2rem * 0.8 / 2);
-          right: calc(-2rem * 0.8 / 2);
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          border-radius: 50%;
-          background-color: red;
-          border: none;
-          color: #fff;
-          font-family:
-            Hiragino Kaku Gothic ProN,
-            Hiragino Sans,
-            Helvetica Neue,
-            Helvetica,
-            Arial,
-            sans-serif;
-          font-size: 1rem;
-          font-weight: 600;
-          height: 2rem;
-          width: 2rem;
-          white-space: nowrap;
-          transform: scale(0.8);
-          transform-origin: 50% 50%;
-        `}
-      >
-        {String(cartTotalQuantity)}
-      </span>
+      {cartTotalQuantity > 0 && (
+        <span
+          className={css`
+            position: absolute;
+            top: calc(-2rem * 0.8 / 2);
+            right: calc(-2rem * 0.8 / 2);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 50%;
+            background-color: red;
+            border: none;
+            color: #fff;
+            font-family:
+              Hiragino Kaku Gothic ProN,
+              Hiragino Sans,
+              Helvetica Neue,
+              Helvetica,
+              Arial,
+              sans-serif;
+            font-size: 1rem;
+            font-weight: 600;
+            height: 2rem;
+            width: 2rem;
+            white-space: nowrap;
+            transform: scale(0.8);
+            transform-origin: 50% 50%;
+          `}
+        >
+          {String(cartTotalQuantity)}
+        </span>
+      )}
       <IconCart width="2.8rem" height="2.8rem" color="#2d2d2d" />
     </Link>
   )
